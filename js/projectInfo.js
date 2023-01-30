d318dc8ed273ac215909bdc9ec7c209969857c2b
burak-sahin156 <burak.sahin.ayvansaray@gmail.com>
2023-01-30 04:10:18 +0300

    ilk yayın

---
+++ js/projectInfo.js
@@ -0,0 +1,57 @@
+const Projects = {
+    Details:{ 
+        HellBridgeRunner: {
+            ID: "HellBridgeRunner",
+            Name: "Hell Bridge Runner Demo",
+            SmallDescription: "Android,Runner,Unity3D",
+            LargeDescription: "",
+            DownloadLink: "",
+            EmbedVideoLinkID: "BridgeRunner",
+            NoOfProjectImages: 7
+        },
+        BattleHero: {
+            ID: "BattleHero",
+            Name: "Battle Hero Demo",
+            SmallDescription: "Android, Arcade, Unity3D",
+            LargeDescription: "",
+            EmbedVideoLinkID: "BattleHero",
+            NoOfProjectImages: 7
+        },
+        SwordRunner: {
+            ID: "SwordRunner",
+            Name: "Sword Runner Demo",
+            SmallDescription: "Android, Endless Runner, Unity3D",
+            LargeDescription: '',
+            DownloadLink: "",
+            EmbedVideoLinkID: "SwordRunner",
+            NoOfProjectImages: 3
+        },
+        CircusCharlie: {
+            ID: "CircusCharlie",
+            Name: "Circus Charlie Demo",
+            SmallDescription: "Published Android Game, Unity3D, C#",
+            LargeDescription: "",
+            DownloadLink: "",
+            EmbedVideoLinkID: "CircusCharlie",
+            NoOfProjectImages: 5
+        },
+        CoffeGameIdea: {
+            ID: "CoffeGameIdea",
+            Name: "Coffe Game Idea Demo",
+            SmallDescription: "Android Game, C#, Hybrid Casual, Unity3D, Idle",
+            LargeDescription: '',
+            DownloadLink: "",
+            EmbedVideoLinkID: "CoffeGameIdea",
+            NoOfProjectImages: 8
+        },        
+        DonerTacoKebapMaster: {
+            ID: "DonerTacoKebapMaster",
+            Name: "Doner Taco Kebap Master",
+            SmallDescription: "Android Game, C#, Unity3D, Hybrid Casual, Idle",
+            LargeDescription: '',
+            DownloadLink: "",
+            EmbedVideoLinkID: "DonerTacoKebapMaster",
+            NoOfProjectImages: 11
+        },
+    }
+};
