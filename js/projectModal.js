51f9858c77edfea235c7c1d2d2060fb04c5a3e45
burak-sahin156 <burak.sahin.ayvansaray@gmail.com>
2023-05-14 03:33:32 +0300

    resim degisikligi

--- js/projectModal.js
+++ js/projectModal.js
@@ -21,7 +21,7 @@ function SetPorfolioElements(){
         element.lastElementChild.firstElementChild.innerHTML= ProjectDetail.Name;
         var elem = element.getElementsByTagName("img")[0];
         element.lastElementChild.lastElementChild.innerHTML= ProjectDetail.SmallDescription;
-        elem.setAttribute("src","img/portfolio/"+ProjectDetail.ID+"/thumb.png".toLowerCase());
+        elem.setAttribute("src","img/portfolio/"+ProjectDetail.ID+"/thumb.png");
         elem.setAttribute("alt", ProjectDetail.ID+", "+ProjectDetail.Name+", "+ProjectDetail.SmallDescription);
         elem.setAttribute("title", ProjectDetail.Name+", "+ProjectDetail.SmallDescription);
         parent.appendChild(element);
